cbead6c7fa0d4d7419d9f1e80a1050bdd6267ce8
AleksandrOgo <ogorodnik.aleksandr@icloud.com>
2023-10-01 22:28:20 +0300

    homepage

---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,13 @@
+import { HomeTitle, WrapperHomePage } from './HomePage.styled';
+
+const HomePage = () => {
+  return (
+    <WrapperHomePage>
+      <HomeTitle>
+       To use the phone book, go through the authorization procedure.
+      </HomeTitle>
+    </WrapperHomePage>
+  );
+};
+
+export default HomePage;
