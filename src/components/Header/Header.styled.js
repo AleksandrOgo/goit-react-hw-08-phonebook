9bd4bf11b84d0866346427c5a263e37493b2205f
AleksandrOgo <ogorodnik.aleksandr@icloud.com>
2023-10-01 22:17:47 +0300

    loader

---
+++ src/components/Header/Header.styled.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const WrapperHeader = styled.header`
+  background: linear-gradient(to bottom, white,#4d514d);
+  padding: 10px;
+  border-bottom: 1px black;
+  box-shadow: rgb(33 33 33) 0px 2px 10px 1px;
+`;
+
+export const WrapperMain = styled.main`
+  display: flex;
+  justify-content: center;
+`;
