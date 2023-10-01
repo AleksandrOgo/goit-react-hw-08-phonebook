21169d6efe8cd6995c7a80f8157bb47e2c820fb6
AleksandrOgo <ogorodnik.aleksandr@icloud.com>
2023-10-01 22:06:29 +0300

    filter

---
+++ src/components/Filter/Filter.styled.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const FilterContainer = styled.label`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 15px;
+`;
+
+export const Field = styled.input`
+  border: 1px solid #434743;
+  cursor: pointer;
+  margin-top: 5px;
+`;
